fcf56b9875f0b6499f0c5f1a37268372e6a4fce2
SdevWeb7 <sd.developper@gmail.com>
2024-07-29 13:01:39 +0200

    Ajout

--- src/app/aide/page.tsx
+++ src/app/aide/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Main from "@/components/main";
 import H1 from "@/components/h1";
 
@@ -5,6 +7,7 @@ export default function Page() {
 
     return <Main>
 
+        <p className={'flex items-center justify-center'}>Paragraphe</p>
         <H1>Demande d&apos;aide</H1>
     </Main>
 }
--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -1,6 +1,7 @@
 import Main from "@/components/main";
 import H1 from "@/components/h1";
 
+
 export default function Page() {
 
     return <Main>
