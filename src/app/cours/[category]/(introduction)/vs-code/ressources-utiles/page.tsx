7e64a1e17a6ef0b14b31847ffa791019f5b28b21
houseOftaz <davoz.anastasia@live.com>
2024-08-15 14:40:31 +0200

    issue fixed

--- src/app/cours/[category]/(introduction)/vs-code/ressources-utiles/page.tsx
+++ src/app/cours/[category]/(introduction)/vs-code/ressources-utiles/page.tsx
@@ -6,24 +6,29 @@ import LICourse from "@/components/cours/li-course";
 import Link from "next/link";
 
 export default function page() {
+  return (
+    <>
+      <H1Course>Ressources utiles</H1Course>
 
-    return <>
-        <H1Course>Ressources utiles</H1Course>
+      <H2Course>Documentation officielle</H2Course>
+      <PCourse>
+        Vous y trouverez des tutoriels et des vidéos expliquant comment utiliser
+        VS Code.
+      </PCourse>
 
-
-        <H2Course>Documentation officielle</H2Course>
-        <PCourse>Vous y trouverez des tutoriels et des vidéos expliquant comment utiliser VS Code.</PCourse>
-
-
-        <UlCourse>
-            <LICourse>
-                <Link href="https://code.visualstudio.com/docs" target="_blank">Documentation officielle</Link>
-                utiliser VS Code.
-            </LICourse>
-            <LICourse>
-                <Link href="www.youtube.com/@code" target="_blank">Chaîne Youtube officielle</Link>
-            </LICourse>
-        </UlCourse>
-
+      <UlCourse>
+        <LICourse>
+          <Link href="https://code.visualstudio.com/docs" target="_blank">
+            Documentation officielle
+          </Link>
+          utiliser VS Code.
+        </LICourse>
+        <LICourse>
+          <Link href="https://www.youtube.com/@code" target="_blank">
+            Chaîne Youtube officielle
+          </Link>
+        </LICourse>
+      </UlCourse>
     </>
+  );
 }
