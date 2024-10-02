fc29a4d84b2ecc9bf369b6017acf9bd01178e150
SdevWeb7 <sd.developper@gmail.com>
2024-10-02 20:00:37 +0200

    fix

--- src/lib/mdx/mdx-remote.tsx
+++ src/lib/mdx/mdx-remote.tsx
@@ -1,13 +1,10 @@
-import {Suspense} from "react";
 import {MDXRemote} from "next-mdx-remote-client/rsc";
 import {MDX_COMPONENTS} from "@/lib/mdx/mdx-components";
 import {rehypePlugin} from "@/lib/mdx/mdx-plugin";
 
 export default function MdxRemote({children}: {children: string}) {
 
-    return <Suspense fallback={<div>Loading ...</div>}>
-
-        <MDXRemote
+    return <MDXRemote
             source={children}
             components={MDX_COMPONENTS}
             options={{
@@ -16,5 +13,4 @@ export default function MdxRemote({children}: {children: string}) {
                 }
             }} />
 
-    </Suspense>
 }
